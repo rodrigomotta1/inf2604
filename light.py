e392f0083e04212c162d3a3fb63d3412bf4c5882
Rodrigo Motta <rodrigmotta@lampsco.com>
2024-06-20 20:47:22 -0300

    Redefine shadow ray creation

--- light.py
+++ light.py
@@ -15,47 +15,27 @@ class Light:
         self.color = color
     
     def radiance(self, world, hit:Hit) -> Tuple[float, np.ndarray]:
-        light_dir:np.ndarray = self.position - hit.point
+        light_dir:np.ndarray = utils.normalize(self.position - hit.point)
         # light_dir:np.ndarray = utils.normalize(self.position - hit.point)
         _light_dist:float = float(np.linalg.norm(self.position - hit.point))
+        shadow_origin:np.ndarray = hit.point + utils.HIT_TOLERANCE * hit.normal
 
-        shadow_ray:Ray = Ray(hit.point, light_dir)
+        shadow_ray:Ray = Ray(shadow_origin, light_dir)
 
         shadow_intersection: Hit | Light | None = world.get_nearest_hit(shadow_ray)
-
-        # if shadow_intersection == self or shadow_intersection is None:
-        #     r:float = utils.distance(hit.point, self.position)
-        #     light_intensity:float = self.power / pow(r, 2)
-
-        #     return (light_intensity, utils.normalize(light_dir))
-        # else:
-        #     return (0.0, colors.BLACK)
-
-        r:float = utils.distance(hit.point, self.position)
-        light_intensity:float = self.power / pow(r, 2)
-
-        return (light_intensity, utils.normalize(light_dir))
-
-        # r = utils.distance(hit.point, self.position)
-        # light_intensity = self.power / pow(r, 2)
-
-        # return (light_intensity,light_dir)
-
-        # Check intersections
-        # shadow_hit = None
-
-        # for hittable in world.hittables:
-        #     shadow_hit:Hit = hittable.intersects(shadow_ray)
-            # if isinstance(hit_evaluation, Light):
-            #     print(f"Light!")
-
-
-            # if shadow_hit is not None:
-            #     if shadow_hit.t < _light_dist:
-            #         return (colors.BLACK, colors.BLACK)
         
+        # Verificação adicional
+        if shadow_intersection is self:
+            r:float = utils.distance(hit.point, self.position)
+            light_intensity:float = self.power / pow(r, 2)
 
-        # return (light_intensity, light_dir)
+            return light_intensity, light_dir
+        else:
+            return 0.0, np.array([0.0, 0.0, 0.0])
+
+
+       
+
     
     def intersects(self, ray:Ray) -> 'Light | None':
         """
@@ -67,8 +47,8 @@ class Light:
         to_light = self.position - ray.origin
         t = np.dot(to_light, ray.direction)
 
-        # if t < 0:
-        #     return None
+        if t < 0:
+            return None
         
         estimated_hit = ray.at(t)
 
@@ -76,6 +56,3 @@ class Light:
             return self
         else:
             return None
-
-        # if pos_at_ray == self.position:
-        #     return True
