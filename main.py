068ebf302421612e7424efde36a72555cce97adc
Rodrigo Motta <rodrigmotta@lampsco.com>
2024-06-20 21:19:03 -0300

    Update last generated image with multi point light sources

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def main():
     world = World(
         hittables=[
             Hittable(
-                surface=Sphere(np.array([-1.1, 0.0, -1.5]), 0.5),
-                material=Material(colors.RED, 0.1, 0.5, 50.0, debug=False)
+                surface=Sphere(np.array([-1.5, 0.0, -1.5]), 0.5),
+                material=Material(colors.RED, 0.1, 0.5, 70.0, debug=False)
             ),
             Hittable(
-                surface=Sphere(np.array([0.0, 0.0, -1.0]), 0.5),
-                material=Material(colors.GREEN, 0.1, 0.5, 50.0, debug=False)
+                surface=Sphere(np.array([0.0, 0.0, -1.5]), 0.5),
+                material=Material(colors.GREEN, 0.1, 0.5, 70.0, debug=False)
             ),
             Hittable(
-                surface=Sphere(np.array([1.1, 0.0, -1.5]), 0.5),
-                material=Material(colors.BLUE, 0.1, 0.5, 50.0, debug=False)
+                surface=Sphere(np.array([1.5, 0.0, -1.5]), 0.5),
+                material=Material(colors.BLUE, 0.1, 0.5, 70.0, debug=False)
             ),
             # Hittable(
             #     surface=Sphere(np.array([0.0, -200.5, -2.0]), 200),
@@ -31,12 +31,13 @@ def main():
             # )
             Hittable(
                 surface=Plane(np.array([0.0, -0.5, 0.0]), np.array([0.0, 1.0, 0.0])),
-                material=Material(np.array([0.7, 0.7, 0.7]), 0.2, 0.5, 50.0, debug=False)
+                material=Material(np.array([0.7, 0.7, 0.7]), 0.2, 0.5, 1000.0, debug=False)
                 # material=Material(colors.WHITE, 0.2, 0.5, 2.0, debug=False)
             )
         ],
         lights=[
-            Light(np.array([0.0, 2.0, -1.5]), 50.0)
+            Light(np.array([0.0, 2.0, -1.5]), 38.0),
+            Light(np.array([6.0, 2.0, -1.5]), 48.0),
         ]
     )
 
